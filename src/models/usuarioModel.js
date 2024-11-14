ba77a43c9e488bcb87c19a3c00b871a99f2ab137
Chagolla03 <quito6404@gmail.com>
2024-11-14 03:17:28 -0600

    Modelo

---
+++ src/models/usuarioModel.js
@@ -0,0 +1,11 @@
+class UsuarioModel {
+  constructor (id, nombre, apellido, correo, contrasena){
+    this.id = id;
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.correo = correo;
+    this.contrasena = contrasena;
+  }
+};
+
+export default UsuarioModel;
